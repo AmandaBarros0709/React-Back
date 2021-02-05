e5406054dd5fc8f3f867ce2a617213c85f31cbbf
AmandaBarros0709 <diinha_92@hotmail.com>
2021-02-05 15:24:18 -0300

    ReactBackIntermediario

---
+++ src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import course from './reducerCourse'
+import roupas from './reducerCarShop'
+
+export default combineReducers({
+    course,
+    roupas, 
+})
